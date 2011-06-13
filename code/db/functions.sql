2b8c8a6e501a9c5b8878ac80eaee1090601802ed
vineelarao <vineelarao@mobisols.com>
2011-06-13 12:39:32 +0000

    functions,triggers tested db updated

--- code/db/functions.sql
+++ code/db/functions.sql
@@ -1,35 +1,35 @@
 DELIMITER $$
 
-DROP FUNCTION IF EXISTS 'GetInfFuture'$$
+DROP FUNCTION IF EXISTS GetInfFuture $$
 
-CREATE FUNCTION 'GetInfFuture'() --function to get infinite future date of the record
+CREATE FUNCTION GetInfFuture() #function to get infinite future date of the record
 	RETURNS DATETIME
 BEGIN
 	DECLARE dInfFuture DATETIME;
-	dInfFuture := '1-1-3000';
+	SET @dInfFuture = '3000-01-01 00:00:00';
 	RETURN dInfFuture;
 END$$
 
 
 
-DROP FUNCTION IF EXISTS 'GetStartDate'$$
+DROP FUNCTION IF EXISTS GetStartDate $$
 
-CREATE FUNCTION 'GetStartDate'() --function to get start date of the record
+CREATE FUNCTION GetStartDate() #function to get start date of the record
 	RETURNS DATETIME
 BEGIN
 	DECLARE dStartDate DATETIME;
-	dStartDate := sysdate();
+	SET @dStartDate = sysdate();
 	RETURN dStartDate;
 END$$
 
 
-DROP FUNCTION IF EXISTS 'GetEndDate'$$
+DROP FUNCTION IF EXISTS GetEndDate$$
 
-CREATE FUNCTION 'GetEndDate'() -- function to get End date of the record
+CREATE FUNCTION GetEndDate() #function to get End date of the record
 	RETURNS DATETIME
 BEGIN
 	DECLARE dEndDate DATETIME;
-	dEndDate := SUBDATE(sysdate(),INTERVAL '1' SECOND);
+	SET @dEndDate = SUBDATE(sysdate(),INTERVAL '1' SECOND);
 	RETURN dEndDate;
 END$$
 
--- code/db/triggers.sql
+++ code/db/triggers.sql
@@ -1,7 +1,9 @@
-create trg_user_payment_bfr_del 
-before delete on user_payment_detail_all as
+create trigger trg_user_payment_bfr_del 
+before delete on user_payment_detail_all 
+for each row
+begin
 update user_payment_detail_history_all
-set end_date := GetEndDate()
+set end_date = GetEndDate()
 where 
 upd_id = old.upd_id
 and end_date = GetInfFuture(); 
@@ -10,36 +12,40 @@ state,country,zip,pay_prefer,action,udf1,udf2,udf3,udf4,udf5,flag1,flag2,flag3,f
 values(null,old.upd_id,old.user_id,old.cc_type_id,old.cc_ac_name,old.cc_number,old.cc_exp_month,old.cc_exp_year,old.cc_cvv,old.bank_routing,old.bank_account,old.address1,old.address2,
 old.city,old.state,old.country,old.zip,old.pay_prefer,'delete',old.udf1,old.udf2,old.udf3,old.udf4,old.udf5,old.flag1,old.flag2,old.flag3,old.flag4,old.flag5,old.created_on,
 old.last_modified_on,old.last_modified_by,GetStartDate(),GetStartDate());
+end;
 -----------------------------------------------------------------------------
 
-create trg_user_payment_aft_ins 
-after insert on user_payment_detail_all as
+create trigger trg_user_payment_aft_ins after insert on user_payment_detail_all 
+FOR EACH ROW
+begin
 insert into user_payment_detail_history_all(updh_id,upd_id,user_id,cc_type_id,cc_ac_name,cc_number,cc_exp_month,cc_exp_year,cc_cvv,bank_routing,bank_account,address1,address2,city,
 state,country,zip,pay_prefer,action,udf1,udf2,udf3,udf4,udf5,flag1,flag2,flag3,flag4,flag5,created_on,last_modified_on,last_modified_by,start_date,end_date) 
 values(null,new.upd_id,new.user_id,new.cc_type_id,new.cc_ac_name,new.cc_number,new.cc_exp_month,new.cc_exp_year,new.cc_cvv,new.bank_routing,new.bank_account,new.address1,new.address2,
 new.city,new.state,new.country,new.zip,new.pay_prefer,'insert',new.udf1,new.udf2,new.udf3,new.udf4,new.udf5,new.flag1,new.flag2,new.flag3,new.flag4,new.flag5,new.created_on,
 new.last_modified_on,new.last_modified_by,GetStartDate(),GetInfFuture());
+end;
 -----------------------------------------------------------------------------
 
-create trg_user_payment_aft_upd 
-after update on user_payment_detail_all as
---update the existing
+create trigger trg_user_payment_aft_upd 
+after update on user_payment_detail_all
+for each row
+begin
 update user_payment_detail_history_all
-set end_date := GetEndDate() 
+set end_date = GetEndDate() 
 where 
 upd_id = new.upd_id
 and end_date = GetInfFuture();
-
---insert the new
 insert into user_payment_detail_history_all(updh_id,upd_id,user_id,cc_type_id,cc_ac_name,cc_number,cc_exp_month,cc_exp_year,cc_cvv,bank_routing,bank_account,address1,address2,city,
 state,country,zip,pay_prefer,action,udf1,udf2,udf3,udf4,udf5,flag1,flag2,flag3,flag4,flag5,created_on,last_modified_on,last_modified_by,start_date,end_date) 
 values(null,new.upd_id,new.user_id,new.cc_type_id,new.cc_ac_name,new.cc_number,new.cc_exp_month,new.cc_exp_year,new.cc_cvv,new.bank_routing,new.bank_account,new.address1,new.address2,
 new.city,new.state,new.country,new.zip,new.pay_prefer,'update',new.udf1,new.udf2,new.udf3,new.udf4,new.udf5,new.flag1,new.flag2,new.flag3,new.flag4,new.flag5,new.created_on,
 new.last_modified_on,new.last_modified_by,GetStartDate(),GetInfFuture());
+end;
 ----------------------------------------------------------------------------
 
-create trg_tol_loc_bfr_del 
-before delete on toll_location_all as
+create trigger trg_tol_loc_bfr_del 
+before delete on toll_location_all 
+for each row begin
 update toll_location_history_all
 set end_date := GetEndDate()
 where 
@@ -47,42 +53,43 @@ toll_location_id = old.toll_location_id
 and end_date = GetInfFuture(); 
 insert into toll_location_history_all(tlh_id,toll_location_id,toll_operator_id,geometry,is_covered,is_cash_only,address1,address2,city,state,country,zip,
 udf1,udf2,udf3,udf4,udf5,flag1,flag2,flag3,flag4,flga5,created_on,last_modified_on,last_modified_by,start_date,end_date,action) 
-values(null,old.tlh_id,old.toll_location_id,old.toll_operator_id,old.geometry,old.is_covered,old.is_cash_only,old.address1,old.address2,old.city,old.state,old.country,old.zip,
+values(null,old.toll_location_id,old.toll_operator_id,old.geometry,old.is_covered,old.is_cash_only,old.address1,old.address2,old.city,old.state,old.country,old.zip,
 old.udf1,old.udf2,old.udf3,old.udf4,old.udf5,old.flag1,old.flag2,old.flag3,old.flag4,old.flag5,old.created_on,old.last_modified_on,old.last_modified_by,
 GetStartDate(),GetStartDate(),'delete');
+end;
 ---------------------------------------------------------------------------
 
-create trg_tol_loc_aft_ins 
-after insert on toll_location_all  as
+create trigger trg_tol_loc_aft_ins 
+after insert on toll_location_all 
+for each row begin
 insert into toll_location_history_all(tlh_id,toll_location_id,toll_operator_id,geometry,is_covered,is_cash_only,address1,address2,city,state,country,zip,
 udf1,udf2,udf3,udf4,udf5,flag1,flag2,flag3,flag4,flga5,created_on,last_modified_on,last_modified_by,start_date,end_date,action) 
-values(null,new.tlh_id,new.toll_location_id,new.toll_operator_id,new.geometry,new.is_covered,new.is_cash_only,new.address1,new.address2,new.city,new.state,new.country,zip,
+values(null,new.toll_location_id,new.toll_operator_id,new.geometry,new.is_covered,new.is_cash_only,new.address1,new.address2,new.city,new.state,new.country,zip,
 new.udf1,new.udf2,new.udf3,new.udf4,new.udf5,new.flag1,new.flag2,new.flag3,new.flag4,new.flag5,new.created_on,new.last_modified_on,new.last_modified_by,
 GetStartDate(),GetInfFuture(),'insert');
+end;
 ---------------------------------------------------------------------------
 
-create trg_tol_loc_aft_upd 
-after update on toll_locations_all as
---update the existing
-
+create trigger trg_tol_loc_aft_upd 
+after update on toll_location_all
+for each row begin
 update toll_location_history_all
-set end_date := GetEndDate()
+set end_date = GetEndDate()
 where 
 toll_location_id = new.toll_location_id
 and end_date = GetInfFuture();
-
---insert the new
 insert into toll_location_history_all(tlh_id,toll_location_id,toll_operator_id,geometry,is_covered,is_cash_only,address1,address2,city,state,country,zip,
 udf1,udf2,udf3,udf4,udf5,flag1,flag2,flag3,flag4,flga5,created_on,last_modified_on,last_modified_by,start_date,end_date,action) 
-values(null,new.tlh_id,new.toll_location_id,new.toll_operator_id,new.geometry,new.is_covered,new.is_cash_only,new.address1,new.address2,new.city,new.state,new.country,new.zip,
+values(null,new.toll_location_id,new.toll_operator_id,new.geometry,new.is_covered,new.is_cash_only,new.address1,new.address2,new.city,new.state,new.country,new.zip,
 new.udf1,new.udf2,new.udf3,new.udf4,new.udf5,new.flag1,new.flag2,new.flag3,new.flag4,new.flag5,new.created_on,new.last_modified_on,new.last_modified_by,
 GetStartDate(),GetInfFuture(),'update');
-
+end;
 --------------------------------------------------------------------------
-create trg_tol_price_bfr_del 
-before delete on toll_price_all as
+create trigger trg_tol_price_bfr_del 
+before delete on toll_price_all
+for each row begin
 update toll_price_history_all
-set end_date := GetEndDate()
+set end_date = GetEndDate()
 where 
 toll_price_id = old.toll_price_id
 and end_date = GetInfFuture(); 
@@ -90,33 +97,34 @@ insert into toll_price_history_all(tph_id,toll_price_id,toll_location_id,vehicle
 flag1,flag2,flag3,flag4,flag5,created_on,last_modified_on,last_modified_by,start_date,end_date)
 values(null,old.toll_price_id,old.toll_location_id,old.vehicle_type_id,old.direction,old.cost_price,old.selling_price,'delete',old.udf1,old.udf2,old.udf3,old.udf4,old.udf5,
 old.flag1,old.flag2,old.flag3,old.flag4,old.flag5,old.created_on,old.last_modified_on,old.last_modified_by,GetStartDate(),GetStartDate());
-
+end;
 ------------------------------------------------------------------------------
-create trg_toll_price_all_aft_ins 
-after insert on toll_price_all  as
+create trigger trg_toll_price_all_aft_ins 
+after insert on toll_price_all 
+for each row begin
 insert into toll_price_history_all(tph_id,toll_price_id,toll_location_id,vehicle_type_id,direction,cost_price,selling_price,action,udf1,udf2,udf3,udf4,udf5,
 flag1,flag2,flag3,flag4,flag5,created_on,last_modified_on,last_modified_by,start_date,end_date)
 values(null,new.toll_price_id,new.toll_location_id,new.vehicle_type_id,new.direction,new.cost_price,new.selling_price,'insert',new.udf1,new.udf2,new.udf3,new.udf4,new.udf5,
 new.flag1,new.flag2,new.flag3,new.flag4,new.flag5,new.created_on,new.last_modified_on,new.last_modified_by,GetStartDate(),GetInfFuture());
-
+end;
 ------------------------------------------------------------------------------
-create trg_toll_price_aft_upd 
-after update on toll_price_all as
---update the existing
+create trigger trg_toll_price_aft_upd 
+after update on toll_price_all
+for each row begin
 update toll_price_history_all
-set end_date := GetEndDate()
+set end_date = GetEndDate()
 where 
 toll_price_id = new.toll_price_id
 and end_date = GetInfFuture();
-
---insert the new
 insert into toll_price_history_all(tph_id,toll_price_id,toll_location_id,vehicle_type_id,direction,cost_price,selling_price,action,udf1,udf2,udf3,udf4,udf5,
 flag1,flag2,flag3,flag4,flag5,created_on,last_modified_on,last_modified_by,start_date,end_date)
 values(null,new.toll_price_id,new.toll_location_id,new.vehicle_type_id,new.direction,new.cost_price,new.selling_price,'update',new.udf1,new.udf2,new.udf3,new.udf4,new.udf5,
 new.flag1,new.flag2,new.flag3,new.flag4,new.flag5,new.created_on,new.last_modified_on,new.last_modified_by,GetStartDate(),GetInfFuture());
+end;
 -------------------------------------------------------------------------------
-create trg_user_all_bfr_del 
-before delete on user_all as
+create trigger trg_user_all_bfr_del 
+before delete on user_basic_all
+for each row begin
 update user_history_all
 set end_date = GetEndDate()
 where 
@@ -126,33 +134,36 @@ insert into user_history_all(user_his_id,user_id,user_name,password,locale,utype
 udf1,udf2,udf3,udf4,udf5,flag1,flag2,flag3,flag4,flag5,created_on,last_modified_on,last_modified_by,start_date,end_date) 
 values(null,old.user_id,old.user_name,old.password,old.locale,old.utype_id,old.last_login_time,old.is_active,old.contact_no,old.client_id,'delete',
 old.udf1,old.udf2,old.udf3,old.udf4,old.udf5,old.flag1,old.flag2,old.flag3,old.flag4,old.flag5,old.created_on,old.last_modified_on,old.last_modified_by,GetStartDate(),GetStartDate());
+end;
 ------------------------------------------------------------------------------
-create trg_user_all_aft_ins 
-after insert on user_all  as
+create trigger trg_user_all_aft_ins 
+after insert on user_basic_all 
+for each row begin
 insert into user_history_all(user_his_id,user_id,user_name,password,locale,utype_id,last_login_time,is_active,contact_no,client_id,action,
 udf1,udf2,udf3,udf4,udf5,flag1,flag2,flag3,flag4,flag5,created_on,last_modified_on,last_modified_by,start_date,end_date) 
 values(null,new.user_id,new.user_name,new.password,new.locale,new.utype_id,new.last_login_time,new.is_active,new.contact_no,new.client_id,'insert',
 new.udf1,new.udf2,new.udf3,new.udf4,new.udf5,new.flag1,new.flag2,new.flag3,new.flag4,new.flag5,new.created_on,new.last_modified_on,new.last_modified_by,GetStartDate(),GetInfFuture());
+end;
 ------------------------------------------------------------------------------
-create trg_user_all_aft_upd 
-after update on user_all as
---update the existing
+create trigger trg_user_all_aft_upd 
+after update on user_basic_all
+for each row begin
 update user_history_all
 set end_date = GetEndDate()
 where 
 user_id = new.user_id
 and end_date =GetInfFuture();
-
---insert the new
 insert into user_history_all(user_his_id,user_id,user_name,password,locale,utype_id,last_login_time,is_active,contact_no,client_id,action,
 udf1,udf2,udf3,udf4,udf5,flag1,flag2,flag3,flag4,flag5,created_on,last_modified_on,last_modified_by,start_date,end_date) 
 values(null,new.user_id,new.user_name,new.password,new.locale,new.utype_id,new.last_login_time,new.is_active,new.contact_no,new.client_id,'update',
 new.udf1,new.udf2,new.udf3,new.udf4,new.udf5,new.flag1,new.flag2,new.flag3,new.flag4,new.flag5,new.created_on,new.last_modified_on,new.last_modified_by,GetStartDate(),GetInfFuture());
+end;
 -------------------------------------------------------------------------------
-create trg_user_vehicle_bfr_del 
-before delete on user_vehicle_detail_all as
+create trigger trg_user_vehicle_bfr_del 
+before delete on user_vehicle_detail_all
+for each row begin
 update user_vehicle_history_all
-set end_date := GetEndDate()
+set end_date = GetEndDate()
 where 
 user_vehicle_id = old.user_vehicle_id
 and end_date = GetInfFuture(); 
@@ -161,49 +172,56 @@ udf1,udf2,udf3,udf4,udf5,flag1,flag2,flag3,flag4,flag5,created_on,last_modified_
 values(null,old.uvh_id,old.user_vehicle_id,old.user_id,old.vehicle_type_id,old.is_active,old.vehicle_start_date,old.vehicle_end_date,old.registration_no,old.registered_state,old.owner_id,
 old.udf1,old.udf2,old.udf3,old.udf4,old.udf5,old.flag1,old.flag2,old.flag3,old.flag4,old.flag5,old.created_on,old.last_modified_on,old.last_modified_by,
 GetStartDate(),GetStartDate(),'delete'));
+end;
 ------------------------------------------------------------------------------
-create trg_user_vehicle_aft_ins 
-after insert on user_vehicle_all  as
+create trigger trg_user_vehicle_aft_ins 
+after insert on user_vehicle_all 
+for each row begin
 insert into user_vehicle_history_all (uvh_id,user_vehicle_id,user_id,vehicle_type_id,is_active,vehicle_start_date,vehicle_end_date,registration_no,registered_state,owner_id,
 udf1,udf2,udf3,udf4,udf5,flag1,flag2,flag3,flag4,flag5,created_on,last_modified_on,last_modified_by,start_date,end_date,action) 
 values(null,new.uvh_id,new.user_vehicle_id,new.user_id,new.vehicle_type_id,new.is_active,new.vehicle_start_date,new.vehicle_end_date,new.registration_no,new.registered_state,new.owner_id,
 new.udf1,new.udf2,new.udf3,new.udf4,new.udf5,new.flag1,new.flag2,new.flag3,new.flag4,new.flag5,new.created_on,new.last_modified_on,new.last_modified_by,
-GetStartDate(),GetInfFuture(),'insert'));
+GetStartDate(),GetInfFuture(),'insert');
+end;
 -----------------------------------------------------------------------------
-create trg_user_vehicle_aft_upd 
-after update on user_vehicle_all as
---update the existing
+create trigger trg_user_vehicle_aft_upd 
+after update on user_vehicle_all
+for each row begin
 update user_vehicle_history_all
-set end_date := GetEndDate()
+set end_date = GetEndDate()
 where 
 user_vehicle_id = new.user_vehicle_id
 and end_date = GetInfFuture();
-
---insert the new
 insert into user_vehicle_history_all(uvh_id,user_vehicle_id,user_id,vehicle_type_id,is_active,vehicle_start_date,vehicle_end_date,registration_no,registered_state,owner_id,
 udf1,udf2,udf3,udf4,udf5,flag1,flag2,flag3,flag4,flag5,created_on,last_modified_on,last_modified_by,start_date,end_date,action) 
 values(null,new.uvh_id,new.user_vehicle_id,new.user_id,new.vehicle_type_id,new.is_active,new.vehicle_start_date,new.vehicle_end_date,new.registration_no,new.registered_state,new.owner_id,
 new.udf1,new.udf2,new.udf3,new.udf4,new.udf5,new.flag1,new.flag2,new.flag3,new.flag4,new.flag5,new.created_on,new.last_modified_on,new.last_modified_by,
-GetStartDate(),GetInfFuture(),'update'));
+GetStartDate(),GetInfFuture(),'update');
+end;
 -------------------------------------------------------------------------------
-create trg_bal_log_bfr_del 
-before delete on user_balance_all as
+create trigger trg_bal_log_bfr_del 
+before delete on user_balance_all
+for each row begin
 insert into user_balance_log_all (ublog_id,ubal_id,delta,timestamp,action,udf1,udf2,udf3,udf4,udf5,flag1,flag2,flag3,flag4,flag5,last_modified_by,last_modified_on,created_on)
 values(null,old.ubal_id,old.balance-old.balance,sysdate(),'delete',old.udf1,old.udf2,old.udf3,old.udf4,old.udf5,old.flag1,old.flag2,old.flag3,old.flag4,old.flag5,
 old.last_modified_by,old.last_modified_on,old.created_on);
+end;
 ------------------------------------------------------------------------------
-create trg_bal_log_aft_ins 
-after insert on user_balance_all as
+create trigger trg_bal_log_aft_ins 
+after insert on user_balance_all
+for each row begin
 insert into user_balance_log_all(ublog_id,ubal_id,delta,timestamp,action,udf1,udf2,udf3,udf4,udf5,flag1,flag2,flag3,flag4,flag5,last_modified_by,last_modified_on,created_on)
 values(null,new.ubal_id,0,sysdate(),'new account created',new.udf1,new.udf2,new.udf3,new.udf4,new.udf5,new.flag1,new.flag2,new.flag3,new.flag4,new.flag5,
 new.last_modified_by,new.last_modified_on,new.created_on);
+end;
 ------------------------------------------------------------------------------
-create trg_bal_log_aft_upd 
-after update on user_balance_all as
+create trigger trg_bal_log_aft_upd 
+after update on user_balance_all
+for each row begin
 insert into user_balance_log_all (ublog_id,ubal_id,delta,ve_timestamp,action,udf1,udf2,udf3,udf4,udf5,flag1,flag2,flag3,flag4,flag5,last_modified_by,last_modified_on,created_on)
 values(null,new.ubal_id,(new.balance-old.balance),sysdate(),'update',new.udf1,new.udf2,new.udf3,new.udf4,new.udf5,new.flag1,new.flag2,new.flag3,new.flag4,new.flag5,
 new.last_modified_by,new.last_modified_on,new.created_on);
-
+end;
 
 
 
