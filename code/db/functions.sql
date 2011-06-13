156c01de6828ad60fe05184e5ff81ca7ab1e62fa
vineelarao <vineelarao@mobisols.com>
2011-06-13 06:36:23 +0000

    Added: comments to functions.sql

--- code/db/functions.sql
+++ code/db/functions.sql
@@ -2,7 +2,7 @@ DELIMITER $$
 
 DROP FUNCTION IF EXISTS 'GetInfFuture'$$
 
-CREATE FUNCTION 'GetInfFuture'()
+CREATE FUNCTION 'GetInfFuture'() --function to get infinite future date of the record
 	RETURNS DATETIME
 BEGIN
 	DECLARE dInfFuture DATETIME;
@@ -14,7 +14,7 @@ END$$
 
 DROP FUNCTION IF EXISTS 'GetStartDate'$$
 
-CREATE FUNCTION 'GetStartDate'()
+CREATE FUNCTION 'GetStartDate'() --function to get start date of the record
 	RETURNS DATETIME
 BEGIN
 	DECLARE dStartDate DATETIME;
@@ -25,7 +25,7 @@ END$$
 
 DROP FUNCTION IF EXISTS 'GetEndDate'$$
 
-CREATE FUNCTION 'GetEndDate'()
+CREATE FUNCTION 'GetEndDate'() -- function to get End date of the record
 	RETURNS DATETIME
 BEGIN
 	DECLARE dEndDate DATETIME;
