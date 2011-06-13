b796c3ba98341f07f5654891b79d35e0b8cb6150
pradeep.asrp@mobisols.com <pradeep.asrp@mobisols.com@8b0831ed-df83-1b15-f511-1849960cf830>
2011-06-13 03:21:46 +0000

    Updated Demo App

--- demo/client/old/resources/js/paidtolls.js
+++ demo/client/old/resources/js/paidtolls.js
@@ -15,7 +15,5 @@ var paidTolls = new Ext.data.Store({
     	    {date:'2011-02-12', amount:'$8.50', location:'George washington bridge, NY', reg: '984TTR2'},
     	    {date:'2011-02-28', amount:'$0.50', location:'1-15S, Escondido', reg: '4GPB522'},
     	    {date:'2011-03-11', amount:'$2.50', location:'1-15N, Escondido', reg: 'SHISM'}
-       
-      
     ]
 });
