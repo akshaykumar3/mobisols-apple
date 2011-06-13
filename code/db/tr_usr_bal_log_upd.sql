d3526cbd2a57c34dcc5fbe4e6267e3d09429170c
vineelarao <vineelarao@mobisols.com>
2011-06-13 06:31:02 +0000

    Added: 1)triggers.sql - set of modified triggers 2)functions.sql - functions needed by triggers

--- code/db/tr_usr_bal_log_upd.sql
+++
@@ -1,5 +0,0 @@
-create trg_bal_log_on_upd after update on user_balance_all as
-start transaction;
-insert into user_balance_log_all (ublog_id,ubal_id,delta,ve_timestamp,action,udf1,udf2,udf3,udf4,udf5,flag1,flag2,flag3,flag4,flag5,last_modified_by,last_modified_on,created_on)
-values(null,new.ubal_id,new.balance-old.balance,sysdate(),'updated',new.udf1,new.udf2,new.udf3,new.udf4,new.udf5,new.flag1,new.flag2,new.flag3,new.flag4,new.flag5,new.last_modified_by,new.last_modified_on,new.created_on);
-commit;
