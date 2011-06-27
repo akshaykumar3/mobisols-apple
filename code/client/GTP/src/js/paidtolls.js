ec6427909af4c17cec4abc66a41e24cf0c8d0f23
pradeep.asrp <pradeep.asrp@mobisols.com>
2011-06-27 16:11:23 +0000

    removed static data. got data from web services and shown in the lists.. coded for request and post webservices.. testing is done for only some webservices.

--- code/client/GTP/src/js/paidtolls.js
+++ code/client/GTP/src/js/paidtolls.js
@@ -24,30 +24,5 @@ var paidTolls = new Ext.data.Store({
 		amount:'$2.50',
 		location:'I-15N, Escondido',
 		reg: '4GPB522'
-	},{
-		date:'2011-01-15',
-		amount:'$1.50',
-		location:'76W, Long beach',
-		reg: '4GPB522'
-	},{
-		date:'2011-01-18',
-		amount:'$5.00',
-		location:'I-95S, Stamford',
-		reg: '984TTR2'
-	},{
-		date:'2011-02-12',
-		amount:'$8.50',
-		location:'George washington bridge, NY',
-		reg: '984TTR2'
-	},{
-		date:'2011-02-28',
-		amount:'$0.50',
-		location:'1-15S, Escondido',
-		reg: '4GPB522'
-	},{
-		date:'2011-03-11',
-		amount:'$2.50',
-		location:'1-15N, Escondido',
-		reg: 'SHISM'
 	}]
 });
