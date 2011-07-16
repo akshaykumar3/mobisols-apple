0ed13e2a97fa65599401b4c18b8f239fb91e1354
pradeep.asrp@mobisols.com <pradeep.asrp@mobisols.com@8b0831ed-df83-1b15-f511-1849960cf830>
2011-07-16 14:30:37 +0000

    Generated custom css file with the specified UI. Need to do the icons yet.

---
+++ code/client/sunpass/src/js/tolloperators.js
@@ -0,0 +1,13 @@
+Ext.regModel('TollOperators',{
+	fields: [{name:'tolloperator'},
+			{name: 'tollperday'},
+			{name:'avgtoll'}]	
+});
+
+var TollsData = new Ext.data.Store({
+	model: 'TollOperators',
+	storeId: 'tollops',
+	data: [{
+		tolloperator: 'Florida Entry Toll', tollperday: '2$', avgtoll: '4$'
+	}]
+});
